98762ea390b1b4af50ea488ef17693b63701cbdd
Matovu Ronald <ronald.matovu@tollandhaben.com>
2020-02-19 21:11:31 +0300

    Create Rooms.js

---
+++ src/pages/Rooms.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Hero from "../components/Hero";
+
+export const Rooms = () => {
+  return (
+    <div>
+      <Hero hero="roomsHero" />
+    </div>
+  );
+};
+
+export default Rooms;
