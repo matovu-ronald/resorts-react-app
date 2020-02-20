58160ab9a105be4873b8480824e02862533da604
Matovu Ronald <ronald.matovu@tollandhaben.com>
2020-02-20 15:19:48 +0300

    Add Aux component

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,10 +2,12 @@ import React from "react";
 import Hero from "../components/Hero";
 import { Link } from "react-router-dom";
 import Banner from "../components/Banner";
+import Services from '../components/Services';
+import Aux from '../hoc/Aux';
 
 export const Home = () => {
   return (
-    <div>
+    <Aux>
       <Hero>
         <Banner
           title="Luxurious Rooms"
@@ -16,7 +18,8 @@ export const Home = () => {
           </Link>
         </Banner>
       </Hero>
-    </div>
+      <Services />
+    </Aux>
   );
 };
 
