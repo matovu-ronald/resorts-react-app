974cfc786f024aaa725e50400cc527b5286e097b
Matovu Ronald <ronald.matovu@tollandhaben.com>
2020-02-21 22:17:44 +0300

    Add room markup

--- src/components/Room.js
+++ src/components/Room.js
@@ -1,10 +1,19 @@
 import React from 'react';
 
-const Room = (props) => {
+const Room = ({room}) => {
+    const { name, slug, images, price } = room;
     return (
-        <div>
-            This is a room component 
-        </div>
+        <article className="room">
+            <div className="img-container">
+                <img src={images[0] || defaultImage } alt={name} />
+                <div className="price-top">
+                    <h6>UGX {price}</h6>
+                    <p>per night</p>
+                </div>
+                <Link to={`/rooms/${slug}`} className="btn-primary room-link" >Features</Link>
+            </div>
+            <p className="room-info">{name}</p>
+        </article>
     )
 }
 
