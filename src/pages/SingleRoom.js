58893ad3199b22fa715098be58a4e5876cabedb2
Matovu Ronald <ronald.matovu@tollandhaben.com>
2020-02-22 12:48:38 +0300

    Add StyleHero component

--- src/pages/SingleRoom.js
+++ src/pages/SingleRoom.js
@@ -1,7 +1,59 @@
 import React, { Component } from "react";
 
+import Hero from "../components/Hero";
+import Banner from "../components/Banner";
+import { Link } from "react-router-dom";
+import { RoomContext } from "../Context";
+
+import defaulBcg from "../images/room-1.jpeg";
+import { FaWizardsOfTheCoast } from "react-icons/fa";
+import StyledHero from "../components/StyledHero";
+
 export default class SingleRoom extends Component {
+  constructor(props) {
+    super(props);
+    // console.log(this.props)
+    this.state = {
+      slug: this.props.match.params.slug,
+      defaulBcg
+    };
+  }
+
+  static contextType = RoomContext;
+
   render() {
-    return <div>Hello from single room component</div>;
+    const { getRoom } = this.context;
+    const room = getRoom(this.state.slug);
+    if (!room) {
+      return (
+        <div className="error">
+          <h3>No such room could be found!</h3>
+          <Link to="/rooms" className="btn-primary">
+            Bact to rooms
+          </Link>
+        </div>
+      );
+    }
+    const {
+      name,
+      description,
+      capacity,
+      size,
+      price,
+      extras,
+      breakfast,
+      pets,
+      images
+    } = room;
+
+    return (
+        <>
+            <StyledHero img={images[0] || this.state.defaulBcg}>
+                <Banner title={`${name} room`}> 
+                    <Link to="/rooms" className="btn-primary">Back to rooms</Link>
+                </Banner>
+            </StyledHero>
+        </>
+    )
   }
 }
